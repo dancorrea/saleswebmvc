6409fc9a83b16aacf97b86692b957aac72548f09
Daniel Correa <danccorrrea@gmail.com>
2022-03-02 11:53:36 -0300

    Validations

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,9 +8,13 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Display(Name = "Birth Date")]
@@ -19,7 +23,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Base Salary")]
-        [DisplayFormat(DataFormatString = "{0:F2")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
